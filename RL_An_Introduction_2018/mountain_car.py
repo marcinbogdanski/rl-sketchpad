56e3d00e2bd6b3733e11c4ce2626a8a476b0d366
Marcin Bogdanski <marcin.bogdanski@dronex.co.uk>
2018-10-17 12:11:51 +0100

    Update 10.1

---
+++ RL_An_Introduction_2018/mountain_car.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MountainCarEnv:
+    """Mountain Car as described in Sutton & Barto (2018), example 10.1"""
+    def __init__(self):
+        self._pos = 0
+        self._vel = 0
+        self.state_low = np.array([-1.2, -0.07])   # min pos, min vel
+        self.state_high = np.array([0.5, 0.07])    # max pos, max vel
+        self.act_space = [0, 1, 2]                 # [left, idle, right]
+        self.reset()
+
+    def reset(self):
+        self._pos = np.random.uniform(-0.6, -0.4)  # start pos
+        self._vel = 0.0
+        self._done = False
+        return np.array([self._pos, self._vel], dtype=float)  # use np arrays everywhere
+
+    def step(self, action):
+        assert self._done == False
+        assert action in self.act_space
+
+        self._vel = self._vel + 0.001*(action-1) - 0.0025*np.cos(3*self._pos)
+        self._vel = min(max(self._vel, -0.07), 0.07)
+
+        self._pos = self._pos + self._vel
+        self._pos = min(max(self._pos, -1.2), 0.5)
+
+        if self._pos == -1.2:
+            self._vel = 0.0
+
+        if self._pos == 0.5:
+            obs = np.array([self._pos, self._vel], dtype=float)
+            reward = -1
+            self._done = True
+            return obs, reward, self._done
+        else:
+            obs = np.array([self._pos, self._vel], dtype=float)
+            reward = -1
+            return obs, reward, self._done

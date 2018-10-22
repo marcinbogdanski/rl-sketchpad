31c7e25d4fa8e1d9aa95b68038fd30443e252c67
Marcin Bogdanski <marcin.bogdanski@dronex.co.uk>
2018-10-22 16:42:40 +0100

    Make compatible with gym mountain car

--- Deep_Q_Network/mountain_car.py
+++ Deep_Q_Network/mountain_car.py
@@ -1,14 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
+    
+
+from collections import namedtuple
+ObsSpace = namedtuple('ObsSpace', 'shape low high')
+
+class BoxSpace():
+    def __init__(self, shape, low, high):
+        assert isinstance(shape, tuple)
+        assert isinstance(low, list)
+        assert isinstance(high, list)
+        self.shape = shape
+        self.low = np.array(low)
+        self.high = np.array(high)
+        
+class DiscreteSpace():
+    def __init__(self, n):
+        assert isinstance(n, int)
+        self.n = n
+    def sample(self):
+        return np.random.randint(self.n)
+    
 
 class MountainCarEnv:
     """Mountain Car as described in Sutton & Barto (2018), example 10.1"""
     def __init__(self):
+        
+        self.observation_space = BoxSpace(
+            shape=(2,), low=[-1.2, -0.07], high=[0.5, 0.07])
+        
+        self.action_space = DiscreteSpace(n=3)
+        
         self._pos = 0
         self._vel = 0
-        self.state_low = np.array([-1.2, -0.07])   # min pos, min vel
-        self.state_high = np.array([0.5, 0.07])    # max pos, max vel
-        self.act_space = np.array([0, 1, 2])       # [left, idle, right]
+        #self.state_low = np.array([-1.2, -0.07])   # min pos, min vel
+        #self.state_high = np.array([0.5, 0.07])    # max pos, max vel
+        #self.act_space = np.array([0, 1, 2])       # [left, idle, right]
         self.reset()
 
     def reset(self):
@@ -19,7 +46,7 @@ class MountainCarEnv:
 
     def step(self, action):
         assert self._done == False
-        assert action in self.act_space
+        assert 0 <= action < self.action_space.n
 
         self._vel = self._vel + 0.001*(action-1) - 0.0025*np.cos(3*self._pos)
         self._vel = min(max(self._vel, -0.07), 0.07)
@@ -38,4 +65,4 @@ class MountainCarEnv:
         else:
             obs = np.array([self._pos, self._vel], dtype=float)
             reward = -1
-            return obs, reward, self._done
+            return obs, reward, self._done, None
